af354e6314253404eb48643a021835b443ef60fa
109703010 <andyszztzn@gmail.com>
2022-05-27 21:08:26 +0800

    Fix some bug

--- code/Stadium.py
+++ code/Stadium.py
@@ -1,6 +1,37 @@
 from kazoo.client import *
+import argparse
 
-zk = KazooClient()
+# The input should avoid using / since it would be seen as next level of directory
+HINT_MSG = ( 
+    "Please format your input as: Stadium Month-Day Team1:Team2 Score1:Score2\n"
+    "Type q to cancel the connection\n"
+    ">>> "
+)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--server", help="IP address of zooKeeper server, default using localhost")
+args = parser.parse_args()
+
+IP = 'localhost'
+if args.server:
+    IP = args.server
+# Assume the same IP address with port 2181~2183
+server_list = IP + ':' + str(2181)
+for i in range(1,3):
+    server_list += (',' + IP + ':' + str(2181+i))
+# print(server_list)
+
+zk = KazooClient(server_list)
 zk.start()
-children = zk.get_children('/')
+
+cmd = input(HINT_MSG)
+while cmd != 'q':
+    data = cmd.split(' ')
+    # print(data)
+    if len(data) == 4:
+        path = '/' + data[0] + '/' + data[1]
+        data = data[2] + '=' + data[3]
+        data = data.encode("utf-8")
+        zk.create(path, data, makepath=True)
+    cmd = input(HINT_MSG)
 zk.stop()
