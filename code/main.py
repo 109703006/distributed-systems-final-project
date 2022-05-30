06c8897daaa532e98605496f299fefcf39918aa7
scw0108 <bruce900108@icloud.com>
2022-05-30 16:40:49 +0800

    大致完成網頁基本元素

--- code/main.py
+++ code/main.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from flask import request
+from flask import request,redirect,url_for
 from flask import render_template
 from flask import session
-from flask_sqlalchemy import SQLAlchemy
+import time
+import threading
 
 app = Flask(__name__, static_folder="static", static_url_path="/")
 
@@ -10,5 +11,16 @@ app = Flask(__name__, static_folder="static", static_url_path="/")
 def index():
     return render_template('index.html')
 
+@app.route("/search", methods=['POST'])
+def search():
+    place=request.form['Stadium']
+    time=request.form['Time']
+    print(place,time)
+    return render_template("index.html", result="Result: ", score="?????????")
+    
+@app.route("/subscribe", methods=['POST'])
+def subscribe():
+    return render_template("index.html", result="Result: ", score="?????????")
+    
 if __name__ == "__main__":
     app.run(port=5000)
