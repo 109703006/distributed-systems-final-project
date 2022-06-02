18a9c2d93055fd9ca7115f1ba161fd9be1f99212
109703006 <109703006@g.nccu.edu.tw>
2022-06-02 18:17:36 +0800

    update getData.py

--- code/getData.py
+++ code/getData.py
@@ -1,8 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get("https://zxc22.idv.tw/sche/main.asp?clickflag=999")
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"
+}
+r = requests.get("https://zxc22.idv.tw/sche/main.asp?clickflag=999", headers=headers)
 r.encoding = "big5"
+# print(r.text)
 
 soup = BeautifulSoup(r.text, "html.parser")
 print(soup.prettify())
+table = soup.find_all("td", align="center", bgcolor="white", valign="top")
+for element in table:
+    print(element)
+for element in table:
+    print(element.getText())
